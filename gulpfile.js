bf874c08067cdb23cb9e1050a335ebd9248ef46e
Thomas Bradford <thom@kode4food.it>
2015-12-13 21:51:52 +0100

    cleanup up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -50,48 +50,43 @@ function buildDir(filename) {
 }
 
 function createUnitTests() {
-  return gulp.src(testFiles).pipe(nodeunit(nodeUnitConfig));
+  return gulp.src(testFiles)
+             .pipe(nodeunit(nodeUnitConfig));
 }
 
 gulp.task('clean', function () {
   return del([buildDir('**/*')]);
 });
 
-gulp.task('prepare', ['clean'], function (done) {
-  gulp.src(jsFiles)
-      .pipe(copy(buildDir(), { prefix: 1 }))
-      .on('end', done);
+gulp.task('prepare', ['clean'], function () {
+  return gulp.src(jsFiles)
+             .pipe(copy(buildDir(), { prefix: 1 }));
 });
 
-gulp.task('parser', function (done) {
-  gulp.src(parserFile)
-      .pipe(pegjs())
-      .pipe(rename(parserOutput))
-      .pipe(gulp.dest(buildDir('compiler')))
-      .on('end', done);
+gulp.task('parser', ['prepare'], function () {
+  return gulp.src(parserFile)
+    .pipe(pegjs())
+    .pipe(rename(parserOutput))
+    .pipe(gulp.dest(buildDir('compiler')));
+});
+
+gulp.task('compile', ['parser'], function() {
+  return gulp.src(tsFiles)
+             .pipe(typescript(tsProject))
+             .js
+             .pipe(gulp.dest('./build'));
+});
+
+gulp.task('test', ['compile'], function () {
+  return createUnitTests();
 });
 
 gulp.task('lint', function() {
   return gulp.src(tsFiles)
-             .pipe(tslint())
-             .pipe(tslint.report('verbose', {
-               summarizeFailureOutput: true
-             }));
-});
-
-gulp.task('compile', ['prepare', 'parser'], function(done) {
-  gulp.src(tsFiles)
-      .pipe(typescript(tsProject))
-      .js
-      .pipe(gulp.dest('./build'))
-      .on('end', done);
-
-//  var tsResult = tsProject.src().pipe(typescript(tsProject));
-//  return tsResult.js.pipe(gulp.dest('.'));
-});
-
-gulp.task('test', ['compile'], function (done) {
-  createUnitTests().on('end', done);
+    .pipe(tslint())
+    .pipe(tslint.report('verbose', {
+      summarizeFailureOutput: true
+    }));
 });
 
 gulp.task('coverage', ['compile'], function (done) {
@@ -103,10 +98,9 @@ gulp.task('coverage', ['compile'], function (done) {
       });
 });
 
-gulp.task('enforce', ['coverage'], function (done) {
-  gulp.src('.')
-      .pipe(enforcer(enforcerConfig))
-      .on('end', done);
+gulp.task('enforce', ['lint', 'coverage'], function () {
+  return gulp.src('.')
+             .pipe(enforcer(enforcerConfig));
 });
 
 gulp.task('watch', ['compile'], function () {
