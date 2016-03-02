ad79253f69d6cc5b633c77d32769c4cf21e834fd
Thomas Bradford <thom@kode4food.it>
2016-03-02 20:09:18 +0100

    adding http lib

--- src/resolvers/system/support.ts
+++ src/resolvers/system/support.ts
@@ -1,5 +1,7 @@
 "use strict";
 
+const Promise = require('welsh').Promise;
+
 const math = Math;
 const object = Object;
 const string = String;
@@ -10,8 +12,13 @@ export {
   object as Object,
   string as String,
   number as Number,
+  createPromise,
   isA
 };
+
+function createPromise(executor: Function) {
+  return new Promise(executor);
+}
 
 function isA(value: any, type: string): boolean {
   return typeof value === type;
