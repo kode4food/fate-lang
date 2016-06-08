c23c8310fe9c335264f2b935004a694de51f0677
Thomas Bradford <thom@kode4food.it>
2016-06-08 09:16:48 +0200

    up coverage

--- gulpfile.js
+++ gulpfile.js
@@ -33,8 +33,8 @@ var nodeUnitConfig = {
 
 var enforcerConfig = {
   thresholds: {
-    statements: 99.87,
-    branches: 97,
+    statements: 99.92,
+    branches: 97.26,
     functions: 100,
     lines: 100
   },
